965f756f699bf4b402bf574e466c13e57135911f
Simeon Rediger <simeonrediger@gmail.com>
2025-12-01 01:03:24 -0500

    Cache form elements

---
+++ index.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+
+const inputs = {
+    email: document.getElementById('email'),
+    country: document.getElementById('country'),
+    postalCode: document.getElementById('postal-code'),
+    password: document.getElementById('password'),
+    passwordConfirmation: document.getElementById('password-confirmation'),
+};
+
+const submitButton = document.querySelector('button');
