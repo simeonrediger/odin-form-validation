d820f1ee53781fa116a41182fd3820b0c012b07f
Simeon Rediger <simeonrediger@gmail.com>
2025-12-04 18:01:12 -0500

    Define stable keys for each field rule

---
+++ field-rules.js
@@ -0,0 +1,8 @@
+const fieldRules = Object.freeze({
+    REQUIRED: 'REQUIRED',
+    EMAIL: 'EMAIL',
+    POSTAL_CODE: 'POSTAL_CODE',
+    PASSWORD_CONFIRMATION: 'PASSWORD_CONFIRMATION',
+});
+
+export default fieldRules;
