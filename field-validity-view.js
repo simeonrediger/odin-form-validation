dcf7ac21224e92071d6f8e868cba39c67fc3b697
Simeon Rediger <simeonrediger@gmail.com>
2025-12-06 15:50:41 -0500

    Cache and validate rulesElement

--- field-validity-view.js
+++ field-validity-view.js
@@ -1,7 +1,16 @@
 export default class FieldValidityView {
     #element;
+    #rulesElement;
 
     constructor(element) {
         this.#element = element;
+        this.#rulesElement = this.#element.nextElementSibling;
+
+        if (!this.#rulesElement.matches("[data-role='requirements']")) {
+            throw new Error(
+                'Requirements element must follow field element:',
+                this.#element,
+            );
+        }
     }
 }
