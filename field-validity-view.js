2939c43c15a96324ba8c774597c5c2bd6acb3c69
Simeon Rediger <simeonrediger@gmail.com>
2025-12-06 16:55:39 -0500

    Initialize rulesElement content on construction

--- field-validity-view.js
+++ field-validity-view.js
@@ -1,12 +1,14 @@
 import fieldRuleSchemas from './field-rule-schemas.js';
+import formFieldSchemas from './form-field-schemas.js';
 
 export default class FieldValidityView {
     #fieldElement;
     #rulesElement;
 
-    constructor(fieldElement) {
+    constructor(fieldElement, field) {
         this.#fieldElement = fieldElement;
         this.#cacheElements();
+        this.#initRulesElement(field);
         this.#positionRulesElement();
     }
 
@@ -21,6 +23,26 @@ export default class FieldValidityView {
         }
     }
 
+    #initRulesElement(field) {
+        this.#rulesElement.innerHTML = '';
+        const rules = formFieldSchemas[field].rules;
+
+        for (const rule of rules) {
+            const ruleStatus = document.createElement('span');
+            ruleStatus.dataset.ruleStatus = '';
+
+            const ruleDescription = document.createElement('span');
+            ruleDescription.textContent = fieldRuleSchemas[rule].description;
+
+            const ruleMessage = document.createElement('p');
+            ruleMessage.dataset.rule = rule;
+            ruleMessage.classList.add('rule');
+            ruleMessage.append(ruleStatus, ruleDescription);
+
+            this.#rulesElement.append(ruleMessage);
+        }
+    }
+
     #positionRulesElement() {
         const formElement = document.querySelector('[data-form]');
         const formRect = formElement.getBoundingClientRect();
@@ -31,18 +53,14 @@ export default class FieldValidityView {
     }
 
     render(validity) {
-        this.#rulesElement.innerHTML = '';
         let firstInvalidRule;
 
         for (const [rule, isValid] of Object.entries(validity)) {
-            const ruleDescription = fieldRuleSchemas[rule].description;
-            const ruleStatus = isValid ? '✅' : '❌';
+            const ruleStatus = this.#rulesElement.querySelector(
+                `[data-rule='${rule}'] [data-rule-status]`,
+            );
 
-            const ruleMessage = document.createElement('p');
-            ruleMessage.classList.add('rule');
-            ruleMessage.textContent = `${ruleStatus} ${ruleDescription}`;
-
-            this.#rulesElement.append(ruleMessage);
+            ruleStatus.textContent = isValid ? '✅' : '❌';
 
             if (!firstInvalidRule && !isValid) {
                 firstInvalidRule = rule;
