c479565dd20f1866340d66dfe631f1e9cbed6697
Simeon Rediger <simeonrediger@gmail.com>
2025-12-06 15:34:23 -0500

    Implement validateField()

--- form-controller.js
+++ form-controller.js
@@ -1,6 +1,12 @@
 import formFields from './form-fields.js';
+import formFieldSchemas from './form-field-schemas.js';
+import formView from './form-view.js';
+import validate from './validate.js';
+
+const fieldContexts = {};
 
 function init() {
+    initFieldContexts();
     validateFields();
 }
 
@@ -8,7 +14,32 @@ function validateFields() {
     Object.values(formFields).forEach(validateField);
 }
 
-function validateField(field) {}
+function validateField(field) {
+    const value = formView.getFieldValue(field);
+    const { rules } = formFieldSchemas[field];
+    const context = fieldContexts[field];
+    const validity = validate(value, rules, context);
+    formView.renderValidity(field, validity);
+}
+
+function initFieldContexts() {
+    const fields = Object.values(formFields);
+
+    for (const field of fields) {
+        const context = {};
+        const dependees = formFieldSchemas[field].dependsOn;
+
+        for (const dependee of dependees) {
+            Object.defineProperty(context, dependee, {
+                get() {
+                    return formView.getFieldValue(dependee);
+                },
+            });
+        }
+
+        fieldContexts[field] = context;
+    }
+}
 
 const formController = {
     init,
